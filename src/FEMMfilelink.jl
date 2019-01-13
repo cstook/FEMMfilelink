d920f0de4f3ac67833a510c155dec15156686d8a
Chris Stook <chris@chrisstook.com>
2019-01-13 12:01:46 -0500

    initial push

---
+++ src/FEMMfilelink.jl
@@ -0,0 +1,46 @@
+module FEMMfilelink
+
+import TOML
+configdict = TOML.parsefile("../FEMMfilelinkconfig.toml")
+const exe = configdict["exe"]
+const ifile = configdict["ifile"]
+const ofile = configdict["ofile"]
+
+function clearfiles()
+    rm(ifile,force=true)
+    rm(ofile,force=true)
+    (ifile,ofile)
+end
+
+function setup()
+    clearfiles()
+    run(`$exe -filelink -windowhide`, wait=false)
+end
+
+function readofile()
+    for i=1:20
+        wait(Timer(.1))
+        isfile(ofile) && break
+    end
+    x = ""
+    if isfile(ofile)
+        open(ofile, read=true) do io
+            x = read(io,String)
+        end
+    end
+    x
+end
+
+function writeifile(luastatment)
+    open(femm_files.ifile, create=true, write=true) do io
+        write(io,luastatment)
+    end
+end
+
+function filelink(luastatment; returntype::Type=Float64)
+    writeifile(luastatment)
+    x = readofile()
+    parse(returntype,match(r"\[(.*)\]",x).captures[1])
+end # module
+
+testfilelink() = filelink("2+2") == 4.0
